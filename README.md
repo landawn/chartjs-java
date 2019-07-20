5679076a479d4974c8b168941e90a4b664552647
landawn <landawn@users.noreply.github.com>
2019-07-20 13:46:43 -0700

    Idea initialization

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # chartjs-java
+
+## Features:
+
+## [Changes](https://github.com/landawn/chartjs-java/blob/master/CHANGES.md)
---
+++ src/main/java/com/landawn/chartjs/App.java
@@ -0,0 +1,11 @@
+package com.landawn.chartjs;
+
+/**
+ * Hello Chartjs!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Chartjs!");
+    }
+}
